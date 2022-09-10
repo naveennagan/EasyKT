33dfce7b9a9316364c12029047fa4abddc6348c5
Abhishek Ranjan <abhiranjan@microsoft.com>
2022-09-10 21:06:25 +0530

    search bot setup

--- src/components/search-bot/SearchBot.jsx
+++ src/components/search-bot/SearchBot.jsx
@@ -1,9 +1,40 @@
 import React from "react";
+import { Input, Image, Item } from "semantic-ui-react";
+import "./SearchBot.css";
 
 export const SearchBot = () => {
-    return (
-        <div className="task-container">
-            <p className="sample-title">Task</p>
-        </div>
-    )
+  const description = [
+    "Cute dogs come in a variety of shapes and sizes. Some cute dogs are cute for their adorable faces, others for their",
+    "tiny stature, and even others for their massive size.",
+  ].join(" ");
+  return (
+    <div className="search-bot">
+      <Item.Group>
+        <Item>
+          <Item.Content>
+            <Item.Header as="a">Cute Dog</Item.Header>
+            <Item.Description>
+              <p>{description}</p>
+              <p>
+                Many people also have their own barometers for what makes a cute
+                dog.
+              </p>
+            </Item.Description>
+          </Item.Content>
+        </Item>
+
+        <Item>
+          <Item.Content>
+            <Item.Header as="a">Cute Dog</Item.Header>
+            <Item.Description content={description} />
+          </Item.Content>
+        </Item>
+
+        <Item>
+          <Item.Content header="Cute Dog" description={description} />
+        </Item>
+
+      </Item.Group>
+    </div>
+  );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
-
 import { EasyKT } from './components/EasyKT';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
