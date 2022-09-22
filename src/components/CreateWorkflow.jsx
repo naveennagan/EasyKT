4c8a998be007dc192da03a7ef82de6791f03ff26
Abhishek Ranjan <abhiranjan@microsoft.com>
2022-09-22 13:09:28 +0530

    fix for cancel and save button

--- src/components/CreateWorkflow.jsx
+++ src/components/CreateWorkflow.jsx
@@ -1,30 +1,9 @@
 import React from "react";
-import { AiOutlineSave } from "react-icons/ai";
-import { IoClose } from "react-icons/io5";
 
 export const CreateWorkflow = (props) => {
   const { onHide } = props;
 
   return (
-    <>
-      <div className="create-workflow-container" id="create-workflow">
-        <button
-          className="close-workflow"
-          onClick={() => {
-            onHide();
-          }}
-        >
-          <IoClose>&#10006;</IoClose>
-        </button>
-        <button
-          className="close-workflow"
-          onClick={() => {
-            onHide();
-          }}
-        >
-          <AiOutlineSave>&#10006;</AiOutlineSave>
-        </button>
-      </div>
-    </>
+      <div className="create-workflow-container" id="create-workflow"></div>
   );
 };
--- src/components/WorkflowForm.jsx
+++ src/components/WorkflowForm.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
-
+import { AiOutlineSave } from "react-icons/ai";
+import { IoClose } from "react-icons/io5";
 export const WorkflowForm = (props) => {
-  const { nodeId } = props;
+//   const { nodeId } = props;
 
   return (
     <div className="h3">
@@ -17,15 +18,24 @@ export const WorkflowForm = (props) => {
       </div>
 
       <div className="mb-3">
-       Node Description
+        Node Description
         <textarea type="text" className="form-control" />
       </div>
-
-      <button type="submit" className="btn btn-primary">
-        Submit
+      <button
+        className="close-workflow"
+        onClick={() => {
+          onHide();
+        }}
+      >
+        <IoClose >&#10006;</IoClose>
       </button>
-      <button type="submit" className="btn btn-primary">
-        Cancel
+      <button
+        className="close-workflow"
+        onClick={() => {
+          onHide();
+        }}
+      >
+        <AiOutlineSave>&#10006;</AiOutlineSave>
       </button>
     </div>
   );
