a86fa39e8ed598c5d7abffda335cd3c783f4f590
vedikagupta <vedikagupta@microsoft.com>
2022-09-22 11:19:07 +0530

    changes in workflow icons

--- src/components/CreateWorkflow.jsx
+++ src/components/CreateWorkflow.jsx
@@ -1,13 +1,30 @@
 import React from "react";
+import { MdSaveAlt } from "react-icons/md";
+import { IoClose } from "react-icons/io5";
 
-export const CreateWorkflow = (props)=>{
-    const { onHide } = props;
+export const CreateWorkflow = (props) => {
+  const { onHide } = props;
 
-    return (
-        <div className="create-workflow-container" id="create-workflow">
-            <button className="close-workflow" onClick = {()=>{
-                onHide();
-            }}>&#10006;</button>
-        </div>
-    )
-}
+  return (
+    <>
+      <div className="create-workflow-container" id="create-workflow">
+        <button
+          className="close-workflow"
+          onClick={() => {
+            onHide();
+          }}
+        >
+          <IoClose>&#10006;</IoClose>
+        </button>
+        <button
+          className="close-workflow"
+          onClick={() => {
+            onHide();
+          }}
+        >
+          <MdSaveAlt>&#10006;</MdSaveAlt>
+        </button>
+      </div>
+    </>
+  );
+};
