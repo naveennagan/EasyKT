2403fe39e1e5cbee2db34e4735682215f167f19e
Abhishek Ranjan <abhiranjan@microsoft.com>
2022-09-24 17:52:03 +0530

    smart converter

--- src/components/smart-converter/SmartConverter.jsx
+++ src/components/smart-converter/SmartConverter.jsx
@@ -3,6 +3,18 @@ import "./SmartConverter.css";
 
 export const SmartConverter = () => {
   return (
-    <div className="search-bot row d-flex justify-content-center p-5"></div>
+    <div className="search-bot row justify-content-center p-5">
+      <div className="file-input">
+        <label for="formFileLg" class="form-label">
+          Upload Markdown Files of your existing projects to generate workflows.
+        </label>
+        <input
+          class="form-control form-control-lg mt-5"
+          id="formFileLg"
+          type="file"
+          placeholder="README.md"
+        />
+      </div>
+    </div>
   );
 };
