798f1c712a2081351369ecf9bcae73c6263706b1
Abhishek Ranjan <abhiranjan@microsoft.com>
2022-09-25 17:20:05 +0530

    fixed styling

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -68,49 +68,35 @@ export const Menu = () => {
   };
 
   return (
-    <>
-      <div className="justify-content-center "></div>
-      <header>
-        <a class="logo" href="/">
-          <img
-            class="logo1"
-            width="60px"
-            height="60px"
-            right="10px"
-            border-radius="50% !important"
-            src="https://bglaw.com/wp-content/uploads/2020/05/avatar-placeholder-550x550.png"
-            alt="logo"
-          />
-        </a>
-
-        <nav>
-          <ul class="nav__links">
-            <li>
-              <a href="#">File</a>
-            </li>
-            <li>
-              <a href="#">Edit</a>
-            </li>
-            <li>
-              <a href="#">Menu</a>
-            </li>
-          </ul>
-        </nav>
-        <input
-          className="input-lg search-rounded px-4"
-          type="text"
-          value={searchInput}
-          onChange={onInputChange}
-          onKeyUp={onEnterPress}
-          placeholder="Search for anything..."
-        />
-        <i
-          className="bi bi-search m-5 font-size-large"
-          onClick={onSearchClick}
-        ></i>
-        <i className="bi bi-x-lg font-size-large" onClick={onSearchClear}></i>
-        <p class="menu cta">Menu</p>
-      </header>
-    </>
+    <div className="ek-menu p-3">
+      <img
+        // class="logo1"
+        width="30px"
+        height="30px"
+        right="10px"
+        border-radius="50% !important"
+        src="https://bglaw.com/wp-content/uploads/2020/05/avatar-placeholder-550x550.png"
+        alt="logo"
+        style={{ float: "left", borderRadius: "50px", margin: "10px" }}
+      />
+      <input
+        className="input-lg search-rounded px-4"
+        type="text"
+        value={searchInput}
+        onChange={onInputChange}
+        onKeyUp={onEnterPress}
+        placeholder="Search for anything..."
+      />
+      <i
+        className="bi bi-search m-5 font-size-large"
+        onClick={onSearchClick}
+      ></i>
+      <i className="bi bi-x-lg font-size-large" onClick={onSearchClear}></i>
+      <i
+        className="bi bi-list font-size-large"
+        style={{ float: "right", margin: "15px", color: "white" }}
+        onClick={onSearchClear}
+      ></i>
+    </div>
   );
 };
